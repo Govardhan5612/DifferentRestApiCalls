6c5edda01d43900f03eaeb62acce22c1aa3f9b72
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-05-30 12:53:46 +0530

    Ability to show the message use put method

--- src/main/java/com/example/differentApiCalls/controller/PersonController.java
+++ src/main/java/com/example/differentApiCalls/controller/PersonController.java
@@ -21,4 +21,8 @@ public class PersonController {
     public String message2(@RequestBody User user){
         return "Hii "+user.firstName+" "+user.lastName+" from BridgeLabz";
     }
+    @PutMapping("/get4/{firstName}")
+    public String message3(@PathVariable String firstName,@RequestParam String lastName){
+        return "Hii "+firstName+" "+lastName+" from BridgeLabz";
+    }
 }
