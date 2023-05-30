fbbf4d7bae6efcaf3d8a7d08c93f53648bed547c
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-05-30 11:58:57 +0530

    Ability to print the method perameter

--- src/main/java/com/example/differentApiCalls/controller/PersonController.java
+++ src/main/java/com/example/differentApiCalls/controller/PersonController.java
@@ -1,6 +1,7 @@
 package com.example.differentApiCalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class PersonController {
     @GetMapping("/get")
     public String message(){
         return "Hello BridgeLabz";
+    }
+    @GetMapping("/get1")
+    public String message(@RequestParam String name){
+        return name;
     }
 }
